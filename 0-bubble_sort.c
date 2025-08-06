3ab822b1c1d9143b1f72b2a36fb012a84df7056a
sarahwcqz <sarah.wacquiez@gmail.com>
2025-08-06 08:35:21 +0200

    beginning of task 0, forgot to switch branch

---
+++ 0-bubble_sort.c
@@ -0,0 +1,26 @@
+#include "sort.h"
+
+/**
+ * bubble_sort - sorts an array of int in ascending order using the Bubble algo
+ * @array: the array to print
+ * @size: size of the array
+ * Return: void
+ */
+void bubble_sort(int *array, size_t size)
+{
+	size_t i = 0, j = 0;
+	int temp = 0;
+
+
+	while (i < size)
+	{
+		if (array[i] > array[i + 1])
+		{
+			temp = array[i];
+			array[i] = array[i + 1];
+			array[i + 1] = temp;
+		}
+		print_array(array, size);
+		i++;
+	}
+}
