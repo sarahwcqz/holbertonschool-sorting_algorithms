b3e5862236c9958d7d7e3858d21389a6d0f5dcf3
sarahwcqz <sarah.wacquiez@gmail.com>
2025-08-07 12:53:01 +0200

    modif on task 3

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -8,26 +8,30 @@
 */
 void quick_sort(int *array, size_t size)
 {
-	size_t i = 0, j = 0, piv = size, temp = 0;		/* indexes initialized*/
-
-/*-------------traverse l'array avec j, si j < pivot, swap i et j -------*/
-	while (j < piv)		/* < ou <= ? Besoin de prendre pivot?*/
-	{
-		if (array[j] <= array[piv])
+	size_t j = 0, piv = array[size -1], temp = 0;		/* indexes initialized*/
+	int i = -1;
+	size_t m;
+/*-------------traverse l'array avec j, si j <= pivot, swap i et j -------*/
+	for (m = 0; m < size; m++)
+	{	
+		while (j < size)
 		{
-			temp = array[i];
-			array[i] = array[j];
-			array[j] = temp;
-			i++;
+			if (array[j] <= array[piv])
+			{
+				i++;
+				temp = array[i];
+				array[i] = array[j];
+				array[j] = temp;
+				print_array(array, size);
+			}
+			j++;
 		}
-		j++;
-	}
-
+		i++;
 /*--------------swap pivot et i ------------------*/
 	temp = array[i];
 	array[i] = array[piv];
 	array[piv] = temp;
-
-/* et maintenant la recursioooooon*/
+	/* et maintenant la recursioooooon*/
 /*         |     <= piv      |     > piv      |     */
+	}
 }
